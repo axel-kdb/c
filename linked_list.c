99f3028d3e16a6cee5b7895016b13292287d8408
axel-kdb <115580874+axel-kdb@users.noreply.github.com>
2025-08-07 03:24:04 +0200

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -11,14 +11,16 @@ int main() {
 
 	for (int i = 0; i < 3; i++) {
 		node *n = malloc(sizeof(node));
-        n->number = i;
-        n->next = list;
-        list = n;
-    }
-    node *ptr = list;
-    while (ptr != NULL) {
-        printf("%i\n", ptr->number);
-        ptr = ptr->next;
-    }
+        	n->number = i;
+        	n->next = list;
+        	list = n;
+	}
+	
+    	node *ptr = list;
+	
+    	while (ptr != NULL) {
+        	printf("%i\n", ptr->number);
+        	ptr = ptr->next;
+    	}
 
 }
